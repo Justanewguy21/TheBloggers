7d89c43aa9f1daeba8fd4cf0be239a53a6a3c0ed
Justanewguy21 <taothanh2020@gmail.com>
2021-10-20 18:26:10 +0700

    continue with TheBloggers

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Post
+# Register your models here
+#admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date_created']
+    list_filter = ['title']
+
+admin.site.register(Post, PostAdmin)
+
 
 # Register your models here.
