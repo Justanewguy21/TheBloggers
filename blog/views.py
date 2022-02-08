d055fd289f5dd504ab9734291d498bf7888d4c47
Justanewguy21 <taothanh2020@gmail.com>
2022-02-08 14:27:25 +0700

    save the code

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,26 @@
-from django import forms
 from django.shortcuts import render, redirect
 from .models import Post
 from .forms import PostForm
 
+
+
 # from blog.admin import Post
 # Create your views here.
 def index(request):
+    #List all posts
     posts = Post.objects.all()
+
     if request.method == 'POST':
         form = PostForm(request.POST)
-
-    # return render(request, 'blogs/index.html',{'posts': posts})
+        # return render(request, 'blogs/index.html',{'posts': posts})
         if form.is_valid():
             instance = form.save(commit=False)
             instance.author = request.user
             instance.save()
-        return redirect('index.html')
+            return redirect('index')
     else:
         form = PostForm()
+
     context = {
         'posts': posts,
         'form': form
--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from django.urls import path
-from django.urls import path
-from .views import sign_up
+from django.contrib.auth import views as  auth_views
+
+from .views import sign_up 
+
 
 urlpatterns = [
-    path('sign_up/', sign_up, name = 'member-sign-up'),
-]
+    path('sign_up/', sign_up, name = 'members-sign-up' ),
+    #path('login/', auth_views.LogoutView.as_view(template_name ='members/login.html'), name = 'member-login' ),
+    path('', auth_views.LoginView.as_view(template_name ='members/login.html'), name = 'member-login' ),
+    path('logout/', auth_views.LogoutView.as_view(template_name ='members/logout.html'), name = 'member-logout' )
+    
+] 
